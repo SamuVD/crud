193bbc505da0fe7a942ee955a44a58da390a9053
SamuVD <samuvv2905@gmail.com>
2024-05-08 08:07:31 -0500

    feat: creando tabla de datos

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+
+import {coders} from './database.js'
+
+const tbody = document.querySelector("tbody");
+
+console.log(coders)
